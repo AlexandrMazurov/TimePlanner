8266f1d74c2d312e67e438b76ec2ebc6ae139687
Alexandr <sasha.drozd-97@yandex.ru>
2020-02-16 17:20:20 +0300

    Initial Commit

---
+++ TimePlannerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimePlannerApp
+//
+//  Created by Александр on 2/16/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
