3b949b3223bf347a770158374f928f7ced782fa3
AlexandrMazurov <sasha.drozd-97@yandex.ru>
2020-02-22 21:50:56 +0300

    TPAPP-1: Setup file architecture

---
+++ TimePlannerApp/Navigation/Flows/ApplicationFlow.swift
@@ -0,0 +1,12 @@
+//
+//  ApplicationFlow.swift
+//  TimePlannerApp
+//
+//  Created by Mazurov, Aleksandr on 2/22/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import Foundation
+
+class ApplicationFlow {
+}
---
+++ TimePlannerApp/Scenes/Launch/ViewModel/LaunchViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LaunchViewModel.swift
+//  TimePlannerApp
+//
+//  Created by Mazurov, Aleksandr on 2/22/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import Foundation
+
+class LaunchViewModel {
+    
+}
