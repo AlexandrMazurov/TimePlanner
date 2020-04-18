3f74847f13a3e890fc23373b47c27d019f17b652
AlexandrMazurov <sasha.drozd-97@yandex.ru>
2020-04-18 20:56:55 +0300

    TPAPP-6: Provide base Task model and repository protocol

---
+++ TimePlannerApp/Models/Task.swift
@@ -0,0 +1,44 @@
+//
+//  Task.swift
+//  TimePlannerApp
+//
+//  Created by Mazurov, Aleksandr on 4/18/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    
+    @objc dynamic var id: String?
+    @objc dynamic var title: String?
+    @objc dynamic var taskDescription: String?
+    @objc dynamic var startTime: Date?
+    @objc dynamic var endTime: Date?
+    let priority = RealmOptional<Int>()
+    let rating = RealmOptional<Int>()
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+}
+
+extension Task {
+    convenience
+    init(id: String,
+         title: String,
+         taskDescription: String,
+         startTime: Date,
+         endTime: Date,
+         priority: Int,
+         rating: Int?) {
+        self.init()
+        self.id = id
+        self.title = title
+        self.taskDescription = taskDescription
+        self.startTime = startTime
+        self.endTime = endTime
+        self.priority.value = priority
+        self.rating.value = rating
+    }
+}
---
+++ TimePlannerApp/Services/RepositoryProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryProtocol.swift
+//  TimePlannerApp
+//
+//  Created by Mazurov, Aleksandr on 4/18/20.
+//  Copyright © 2020 AlexandrMazurov. All rights reserved.
+//
+
+import Foundation
+
+protocol RepositoryProtocol {
+    
+}
